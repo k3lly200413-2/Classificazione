d0f4d621beaa438f67e632fa7288c5c9d43161ab
Kelly <kelly.applebee@studio.unibo.it>
2026-04-21 16:21:33 +0200

    feat: exercise 1

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
         urlretrieve(BCWDS_URL, "wdbc.data")
     bcwds = pd.read_csv("wdbc.data", header=None, index_col=0)
     
-    print(bcwds.head(3))
+    # print(bcwds.head(3))
     
     cell_features = [
         "radius",     "texture",
@@ -31,7 +31,22 @@ def main():
 
     pd.options.display.max_columns = 31
     
-    print(bcwds.head(5))
+    # print(bcwds.head(5))
+    
+    bMSpread = bcwds["diagnosis"].value_counts()
+    bcwds["mean_area"].plot.hist(bins=20, grid=True)
+    bcwds.plot.scatter("mean_area", "mean_concave_pts")
+    
+    # meanAreaSpread.plot.hist(grid=True, bins=20)
+    
+    # bMSpread.plot.pie()
+    
+    # iloc [row number, coulmn number]
+    print(bcwds.iloc[:, 1:11].describe())
+    
+    print(bMSpread)
+    
+    plt.show()
 
 if __name__ == "__main__":
     main()
