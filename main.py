9706ba03365e59f1b727bfbee96ef33100bf3e26
Kelly <kelly.applebee@studio.unibo.it>
2026-04-21 15:49:52 +0200

    chore: init

---
+++ main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+from os import path
+from urllib.request import urlretrieve
+
+
+def main():
+    # breast cancer wisconson data set
+    BCWDS_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+
+
+    if not path.exists("wdbc.data"):
+        urlretrieve(BCWDS_URL, "wdbc.data")
+    bcwds = pd.read_csv("wdbc.data", header=None, index_col=0)
+    
+    print(bcwds.head(3))
+    
+    cell_features = [
+        "radius",     "texture",
+        "perimeter",  "area",
+        "smoothness", "compactness",
+        "concavity",  "concave_pts",
+        "symmetry",   "fractal_dim",
+    ]
+    
+    stats = ["mean", "std", "worst"]
+    
+    bcwds.columns = ["diagnosis"] + [f"{stat}_{feat}" for stat in stats for feat in cell_features]
+
+    pd.options.display.max_columns = 31
+    
+    print(bcwds.head(5))
+
+if __name__ == "__main__":
+    main()
